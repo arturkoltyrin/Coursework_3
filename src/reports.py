212c01f414f11108a03f68cbc91de7ff33ab7d37
Artur Koltyrin <koltyrinartur@gmail.com>
2024-11-13 22:28:34 +0500

    переписаны декоратор recording_data и функция spending_by_category

--- src/reports.py
+++ src/reports.py
@@ -7,7 +7,6 @@ from typing import Callable, Optional
 import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 rel_file_path = os.path.join(current_dir, "../logs/reports.log")
 abs_file_path = os.path.abspath(rel_file_path)
 
@@ -20,12 +19,17 @@ logger.addHandler(file_handler)
 
 
 def recording_data(file_name: str = "default_report.json") -> Callable:
-    """Декоратор, который записывает в файл результат, который возвращает функция, формирующая отчет"""
+    """Декоратор, который записывает в файл результат, который возвращает функция, формирующая отчет."""
 
     def decorator(func) -> Callable:
-        def wrapper(*args, **kwargs) -> json:
+        def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            result.to_json(path_or_buf=file_name, orient="records", force_ascii=False, indent=4)
+            # Проверка, является ли результат числом
+            if isinstance(result, (int, float)):
+                with open(file_name, "w", encoding="utf-8") as f:
+                    json.dump({"Сумма": result}, f, ensure_ascii=False, indent=4)
+            else:
+                result.to_json(path_or_buf=file_name, orient="records", force_ascii=False, indent=4)
             return result
 
         return wrapper
@@ -33,18 +37,26 @@ def recording_data(file_name: str = "default_report.json") -> Callable:
     return decorator
 
 
-@recording_data()
-def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
-    """Возвращает траты по заданной категории за последние три месяца (от переданной даты)"""
+@recording_data("spending_report.json")
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> float:
+    """Возвращает траты по заданной категории за последние три месяца (от переданной даты)."""
     logger.info("Ищем траты по конкретной категории")
     if date is None:
-        date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-    date = datetime.strptime(date, "%d-%m-%Y %H:%M:%S")
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     three_months_ago = date - pd.DateOffset(months=3)
+
+    # Преобразование столбца "Дата платежа" в формат datetime
     transactions["Дата платежа"] = pd.to_datetime(transactions["Дата платежа"], format="%d.%m.%Y")
+
+    # Фильтрация операций по категории и дате
     filtered_operations = transactions[
         (transactions["Категория"] == category) &
         (three_months_ago <= transactions["Дата платежа"]) &
         (transactions["Дата платежа"] <= date)
         ]
-    return filtered_operations
+
+    # Возвращаем сумму операций с округлением
+    total_sum = filtered_operations["Сумма операции"].sum()
+
+    return round(total_sum, 2)
--- src/views.py
+++ src/views.py
@@ -1,13 +1,16 @@
 import json
 
-from src.utils import (currency_rates, get_data_from_excel, greeting_user,
-                       operations_cards, sort_date_operations, stock_prices,
-                       top_five_transactions)
+import pandas as pd
+
+from src.utils import (convert_timestamps_to_strings, currency_rates, filter_date_operations, greeting_user,
+                       operations_cards, stock_prices, top_five_transactions)
 
 
-def web_main(date):
-    operations = get_data_from_excel("../data/operations.xlsx")
-    operations = sort_date_operations(operations, date)
+def web_main(date: str) -> json:
+    """Возвращает JSON для главной Веб-страницы"""
+    operations = pd.read_excel("../data/operations.xlsx")
+    operations = filter_date_operations(operations, date)
+    operations = convert_timestamps_to_strings(operations)
     result = {
         "greeting": greeting_user(),
         "cards": operations_cards(operations),
@@ -15,5 +18,4 @@ def web_main(date):
         "currency_rates": currency_rates(),
         "stock_prices": stock_prices(),
     }
-
     return json.dumps(result, ensure_ascii=False)
